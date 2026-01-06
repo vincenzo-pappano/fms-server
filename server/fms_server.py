b85575b854fcc682271ad5b710a937ce37e6d6c1
vincenzo <vincenzo.pappano@gmail.com>
2026-01-06 13:18:00 -0500

    Add authorizaton handler

--- server/fms_server.py
+++ server/fms_server.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from .config import BaseConfiguration, DevelopmentConfiguration
 from .handlers.health_handler import HealthHandler
+from .handlers.authorize_handler import AuthorizeHandler
 
 
 class FmsServer(Flask):
@@ -11,8 +12,10 @@ class FmsServer(Flask):
         self.logger.info("Fms Server: Config")
         
         self.health_handler = HealthHandler()
+        self.authorize_handler = AuthorizeHandler()
         
         self.register_routes()
         
     def register_routes(self):
         self.add_url_rule('/health', 'health', self.health_handler.health, methods=["GET"])
+        self.add_url_rule('/authorize', 'authorize', self.authorize_handler.authorize, methods=["GET","POST"])
