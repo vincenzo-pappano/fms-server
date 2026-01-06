92a932b541ee3b7bc822ae3d6c4db41dfd657a4d
vincenzo <vincenzo.pappano@gmail.com>
2026-01-06 11:28:44 -0500

    Run flask server, add sample add_url_rule()

--- server/__main__.py
+++ server/__main__.py
@@ -1,11 +1,16 @@
 from .config import DevelopmentConfiguration
 from .fms_server import FmsServer
+from typing import cast
 
 
 def main():
 
     server = FmsServer(DevelopmentConfiguration())
-    server.logger.info("Main: FMS Server created")    
+    server.logger.info("Main: FMS Server created")      
+    server.run(host=cast(str, server.config["HOST"]), 
+              port=cast(int,server.config["PORT"]), 
+              debug=cast(bool, server.config["DEBUG"]))
+
     
 if __name__ == "__main__":
     main()
--- server/fms_server.py
+++ server/fms_server.py
@@ -1,5 +1,11 @@
 from flask import Flask
+from flask.typing import ResponseReturnValue
 from .config import BaseConfiguration, DevelopmentConfiguration
+
+
+def health() -> ResponseReturnValue:
+    return {"info":"HEALTH Page"}, 200
+    
 
 
 class FmsServer(Flask):
@@ -8,3 +14,8 @@ class FmsServer(Flask):
         super().__init__(__name__)
         self.config.from_object(config)
         self.logger.info("Fms Server: Config")
+        
+        self.register_routes()
+        
+    def register_routes(self):
+        self.add_url_rule('/health', 'health', health, methods=["GET"])
