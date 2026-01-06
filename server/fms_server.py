4c63fbdcfd2f3d5b20e4b1fb2dfb7fb65c3a586c
vincenzo <vincenzo.pappano@gmail.com>
2026-01-06 17:53:23 -0500

    add error handling to /authorize route, class EndPoint to fms_server

--- server/fms_server.py
+++ server/fms_server.py
@@ -1,8 +1,20 @@
-from flask import Flask
+from flask import Flask, make_response, Response
 from server.config import BaseConfiguration, DevelopmentConfiguration
 from server.handlers.health_handler import HealthHandler
 from server.handlers.authorize_handler import AuthorizeHandler
 from server.extensions import dram
+from typing import Callable, Any
+
+
+class EndPointAction:
+
+    def __init__(self, action: Callable[..., Any]) -> None:
+        self.action = action
+
+    def __call__(self, *args: tuple[Any, ...], **kwargs: dict[str, Any]) -> Response:
+        return make_response(self.action(*args, **kwargs))
+
+
 
 class FmsServer(Flask):
 
@@ -19,5 +31,13 @@ class FmsServer(Flask):
         self.register_routes()
         
     def register_routes(self):
-        self.add_url_rule('/health', 'health', self.health_handler.health, methods=["GET"])
-        self.add_url_rule('/authorize', 'authorize', self.authorize_handler.authorize, methods=["GET","POST"])
+        self.add_endpoint('/health',    self.health_handler.health,       ["GET"])
+        self.add_endpoint('/authorize', self.authorize_handler.authorize, ["GET","POST"])
+
+
+    def add_endpoint(self, rule: str, handler: Callable[..., Any], methods: list[str]) -> None:
+        # Parameters for app.add_url_rule(): 
+        #    rule: A string representing the URL rule (e.g., '/', '/user/<username>').
+        #    endpoint: The name for the route, used for URL generation with the url_for() function. If not provided, it defaults to the name of the view_func.
+        #    view_func: The actual Python function to be called when a request matches the URL rule 
+        self.add_url_rule(rule=rule, endpoint=rule.removeprefix('/'), view_func=EndPointAction(handler), methods=methods)
